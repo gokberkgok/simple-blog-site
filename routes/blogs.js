bf16cb3b85cc48f8cfbf4679e12585a9599f80d7
gokberkgok <gokberkgok2004@gmail.com>
2024-09-09 19:26:44 +0300

    09.09.2024 Post Filter System blogs.js - added categories db query and rendered -added a filter system that can filtered the posts on page
    blogs.ejs
    - Added category section for choose

--- routes/blogs.js
+++ routes/blogs.js
@@ -10,21 +10,27 @@ router.get('', async (req,res) => {
         description : "Simple blog site made by - Gokberk"
     }
     res.render('blogs', { locals } )*/
-
+    const filter = req.query.filter;  // Kategoriyi URL'den alıyoruz.
+    const query = filter && filter !== null ? 'SELECT * FROM blogs WHERE category = ?' : 'SELECT * FROM blogs'
     try {
         // SQL sorgusu ile blog verilerini çekin
-        const [blogs] = await db.query('SELECT * FROM blogs');
+        const [blogs] = filter && filter !== null ? await db.query(query, [filter]) : await db.query('SELECT * FROM blogs');
+        const [categories] = await db.query('SELECT id, name FROM categories');
         blogs == null || blogs == "" ? console.log("blog yok") : ""
         // blogs verisini admin.ejs'ye gönderin
-        res.render('blogs', { blogs: blogs });
+        res.render('blogs', { blogs, categories,  selectedCategory: filter || 'All' });
     } catch (error) {
         console.error('Veritabanı hatası:', error);
         res.status(500).send('Veritabanı hatası');
     }
+   
 })
 
 router.get('/blogs', (req,res) => {
    
+})
+router.get('/blogs', async (req,res) => {
+  
 })
 
 module.exports = router;
